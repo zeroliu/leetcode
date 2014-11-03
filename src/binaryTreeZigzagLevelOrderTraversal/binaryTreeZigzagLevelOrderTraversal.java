c0cd20cfc8815919e9116654f5d960ba2917f308
zeroliu <zerolxy@gmail.com>
2014-11-02 16:13:15 -0800

    binaryTreeZigzagLevelOrderTraversal

---
+++ src/binaryTreeZigzagLevelOrderTraversal/binaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        LinkedList<List<Integer>> results = new LinkedList<List<Integer>>();
+        if (root == null) {
+            return results;
+        }
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+        LinkedList<TreeNode> buffer = new LinkedList<TreeNode>();
+        List<Integer> current = new LinkedList<Integer>();
+        boolean isReverse = false;
+        queue.add(root);
+        while (queue.size() > 0) {
+            TreeNode n = queue.pop();
+            if (isReverse) {
+                ((LinkedList<Integer>)current).addFirst(n.val);
+            } else {
+                current.add(n.val);
+            }
+            if (n.left != null) {
+                buffer.add(n.left);
+            }
+            if (n.right != null) {
+                buffer.add(n.right);
+            }
+            if (queue.size() == 0) {
+                queue = buffer;
+                buffer = new LinkedList<TreeNode>();
+                results.add(current);
+                current = new LinkedList<Integer>();
+                isReverse = !isReverse;
+            }
+        }
+        return results;
+    }
+}
