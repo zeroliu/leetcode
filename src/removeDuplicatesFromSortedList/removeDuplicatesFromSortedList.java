0f288a309916518a1df4993ace64301ea136f76e
zeroliu <zerolxy@gmail.com>
2014-11-17 14:00:58 -0800

    removeDuplicatesFromSortedList

---
+++ src/removeDuplicatesFromSortedList/removeDuplicatesFromSortedList.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return head;
+        ListNode current = head;
+        while (current.next != null) {
+            if (current.next.val == current.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+}
