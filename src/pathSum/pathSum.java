6f5d84faddde0f80e78c0b530e777c282638c756
zeroliu <zerolxy@gmail.com>
2014-12-04 22:06:07 -0800

    pathSum

---
+++ src/pathSum/pathSum.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) { return false;}
+        if (root.left == null && root.right == null && sum == root.val) {
+            return true;
+        }
+        if (hasPathSum(root.left, sum-root.val)) { return true; }
+        if (hasPathSum(root.right, sum-root.val)) { return true; }
+        return false;
+    }
+}
